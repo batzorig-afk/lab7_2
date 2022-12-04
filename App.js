b39fe10016e82ca662d7ffbd6f39f65a81339b9b
B200910022 <99472749+batzorig-afk@users.noreply.github.com>
2022-12-04 20:13:51 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+state = {
+myState: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, used do eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ex ercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo r in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excep teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit ani m id est laborum.'
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+updateState = () => this.setState({ myState: 'The state is updated' }) 
+render() {
+return (
+      <View>
+<Text onPress={this.updateState}> 
+{this.state.myState}
+        </Text>
+      </View>
+); 
+}
+}
